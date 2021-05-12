35eae76a378bbb7fdf15aafe0911d6ef328d72f1
Mi-Gyeong <rmfosem613@gmail.com>
2021-05-12 20:29:53 +0900

    Server

---
+++ Server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const colors = require('colors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json({}));
+app.use(express.json({extended:true}))
+
+dotenv.config({
+    path: './config/config.env'
+});
+
+connectDB();
+
+// https://bangidaapp.herokuapp.com/api/bangida/auth/register
+app.use('/api/bangida/auth', require('./routes/user'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, 
+    console.log(`Server running on port : ${PORT}`.red.underline.bold)
+    
+);
