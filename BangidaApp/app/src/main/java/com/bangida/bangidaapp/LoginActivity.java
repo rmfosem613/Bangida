901625e61a1fded53b9ef497ba831c2fec43fd08
Mi-Gyeong <rmfosem613@gmail.com>
2021-05-13 18:46:19 +0900

    Register

---
+++ BangidaApp/app/src/main/java/com/bangida/bangidaapp/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.bangida.bangidaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button registerBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        registerBtn = findViewById(R.id.registerBtn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
