adb5d576e42fa97e5fe30283d46c65078c4fd774
Mi-Gyeong <rmfosem613@gmail.com>
2021-05-14 00:00:27 +0900

    update NavigationBar

--- BangidaApp/app/src/main/java/com/bangida/bangidaapp/MainActivity.java
+++ BangidaApp/app/src/main/java/com/bangida/bangidaapp/MainActivity.java
@@ -1,35 +1,119 @@
 package com.bangida.bangidaapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 
 import com.bangida.bangidaapp.UtilsService.SharedPreferenceClass;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button logout;
     SharedPreferenceClass sharedPreferenceClass;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logout = findViewById(R.id.logout);
         sharedPreferenceClass = new SharedPreferenceClass(this);
 
-        // 로그아웃 버튼 클릭시 token을 삭제하고, LoginActiivity로 이동
-        logout.setOnClickListener(new View.OnClickListener() {
+        // MainActivity
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+        // error
+//        setSupportActionBar(toolbar);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onNavigationItemSelected(@Nullable MenuItem item) {
+                setDrawerClick(item.getItemId());
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        initDrawer();
+
+    }
+
+    private void initDrawer() {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.replace(R.id.content, new HomeFragment());
+        ft.commit();
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+        drawerLayout.addDrawerListener(drawerToggle);
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private void setDrawerClick(int itemId) {
+        switch (itemId) {
+            case R.id.action_finished_task:
+                getSupportFragmentManager().beginTransaction().replace(R.id.content, new FinishedTaskFragment()).commit();
+                break;
+            case R.id.action_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.content, new HomeFragment()).commit();
+                break;
+            case R.id.action_logout:
                 sharedPreferenceClass.clear();
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
                 finish();
-            }
-        });
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@Nullable MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
