1f59d218015ea9849005b0e9a4642ccae8022d67
HYOBIN <52150400+lottt105@users.noreply.github.com>
2021-05-19 15:38:13 +0900

    acbook layout

--- BangidaApp/app/src/main/kotlin/com/bangida/bangidaapp/ui/acbook/AcbookFragment.kt
+++ BangidaApp/app/src/main/kotlin/com/bangida/bangidaapp/ui/acbook/AcbookFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bangida.bangidaapp.databinding.FragmentAcbookBinding
 
@@ -30,10 +28,7 @@ class AcbookFragment : Fragment() {
         _binding = FragmentAcbookBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textAcbook
-        acbookViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return root
     }
 
